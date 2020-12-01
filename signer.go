4fb3f6bc3cc5b850c728f2a7b8a3f03bc7b84ebc
Saygin_DI <Saygin_DI@cd.local>
2020-12-01 17:49:54 +0300

    hw2

--- signer.go
+++ signer.go
@@ -1,3 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"time"
+	"sync/atomic"
+)
+
+func main() {
+	var ok = true
+	var recieved uint32
+	freeFlowJobs := []job{
+		job(func(in, out chan interface{}) {
+			out <- 1
+			time.Sleep(10 * time.Millisecond)
+			currRecieved := atomic.LoadUint32(&recieved)
+			// в чем тут суть
+			// если вы накапливаете значения, то пока вся функция не отрабоатет - дальше они не пойдут
+			// тут я проверяю, что счетчик увеличился в следующей функции
+			// это значит что туда дошло значение прежде чем текущая функция отработала
+			if currRecieved == 0 {
+				ok = false
+			}
+		}),
+		job(func(in, out chan interface{}) {
+			for _ = range in {
+				atomic.AddUint32(&recieved, 1)
+			}
+		}),
+	}
+	ExecutePipeline(freeFlowJobs...)
+	if !ok || recieved == 0 {
+		fmt.Print("no value free flow - dont collect them")
+	}
+}
+
 // сюда писать код
+func ExecutePipeline(in ...job) {
+	
+}
