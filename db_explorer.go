588bcb0c0e2b5eb49214ab0795051778f13d7f4a
orshk <sdi@proekt08.ru>
2021-01-09 19:56:16 +0300

    DBExplorer http

--- db_explorer.go
+++ db_explorer.go
@@ -1,4 +1,26 @@
 package main
 
-// тут вы пишете код
-// обращаю ваше внимание - в этом задании запрещены глобальные переменные
+import (
+	"net/http"
+)
+
+func NewDbExplorer(db) DbExplorer, error {
+	return DbExplorer{db}
+}
+
+type DbExplorer struct {
+	DB *DB
+}
+
+// API
+func (h *DbExplorer) serveHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/user/profile":
+		//h.handlerMyApiProfile(w, r)
+	case "/user/create":
+		//h.handlerMyApiCreate(w, r)
+
+	default:
+		//getResponse(w, ApiError{http.StatusNotFound, fmt.Errorf("unknown method")})
+	}
+}
