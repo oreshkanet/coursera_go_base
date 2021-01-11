22b65e951676fe6881f5eba4957213f2a73c3701
orshk <sdi@proekt08.ru>
2021-01-11 05:23:20 +0300

    ренэйминг

--- db_explorer.go
+++ db_explorer.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func NewDbExplorer(db) DbExplorer, error {
-	return DbExplorer{db}
+// Функция получения экземпляра db_explorer
+func NewDbExplorer(db *sql.DB) (*DbExplorer, error) {
+	dbExplorer := &DbExplorer{db}
+
+	// TODO: тут нужно получить и закешировать список доступных таблиц
+
+	return dbExplorer, nil
 }
 
+// DbExplorer - динамическая работа с БД
 type DbExplorer struct {
-	DB *DB
+	DBConnection *sql.DB
 }
 
 // API
-func (h *DbExplorer) serveHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *DbExplorer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/user/profile":
 		//h.handlerMyApiProfile(w, r)
