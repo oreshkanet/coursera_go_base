5723cf7b6a91b6e2a9464b2a5dd5c25f90a102a7
Saygin_DI <Saygin_DI@cd.local>
2021-01-25 14:21:28 +0300

    Closing server

--- service.go
+++ service.go
@@ -11,12 +11,31 @@ import (
 	"google.golang.org/grpc"
 )
 
+type serverGRPC struct {
+	Server *grpc.Server
+	lis    net.Listener
+}
+
+var (
+	srvGRPC serverGRPC = serverGRPC{
+		Server: grpc.NewServer(),
+	}
+)
+
 // StartMyMicroservice - Старт микросервиса
 func StartMyMicroservice(ctx context.Context, listenAddr string, ACLData string) error {
+
 	lis, err := net.Listen("tcp", ":8082")
 	if err != nil {
 		return err
 	}
+
+	// В отдельной горутине запускаем чтение из контекста признака закрытия сервера
+	go func(_lis net.Listener) {
+		<-ctx.Done()
+		fmt.Println("stop server at :8082")
+		_lis.Close()
+	}(lis)
 
 	server := grpc.NewServer()
 
@@ -39,7 +58,11 @@ type adminManager struct {
 }
 
 func (*adminManager) Logging(nothing *Nothing, server Admin_LoggingServer) error {
-	return nil
+	for {
+		out := &Event{}
+		server.Send(out)
+		return nil
+	}
 }
 
 func (*adminManager) Statistics(statInterval *StatInterval, server Admin_StatisticsServer) error {
